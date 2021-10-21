bddcf4410e752f2010a55edf5c77625498aaee6d
Gaël <gaelduclocher.rougelot@gmail.com>
2021-10-21 14:02:11 +0200

    vue init

---
+++ my-copycat/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="">test</h1>
+  </div>
+</template>
+
+<script>
+import './assets/tailwind.css';
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+};
+</script>
