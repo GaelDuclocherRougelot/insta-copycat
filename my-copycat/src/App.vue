694ba6061ae282a277b579aec7bd840a9a76b0b6
Gaël <gaelduclocher.rougelot@gmail.com>
2021-10-21 17:07:49 +0200

    add header part1

--- my-copycat/src/App.vue
+++ my-copycat/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <h1 class="">test</h1>
+    <Header />
+    <div class="h-screen"></div>
   </div>
 </template>
 
 <script>
 import './assets/tailwind.css';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-
+    Header,
   },
 };
 </script>
---
+++ my-copycat/src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="header flex justify-center items-center border-b border-border-primary">
+    <div class="container flex justify-between items-center px-5 w-full">
+      <div class="container-img flex justify-between items-center">
+        <img class="mt-2" src="../assets/logo.png" alt="instagram logo">
+      </div>
+      <div class="container-search">
+        <input type="text" class="border border-border-primary w-full">
+      </div>
+      <div class="container-nav flex justify-between">
+        <img :src="require(`../assets/svg/insta/svgexport-${svg}.svg`)" alt="logo">
+        <img :src="require(`../assets/svg/insta/svgexport-${svg}.svg`)" alt="logo">
+        <img :src="require(`../assets/svg/insta/svgexport-${svg}.svg`)" alt="logo">
+        <img :src="require(`../assets/svg/insta/svgexport-${svg}.svg`)" alt="logo">
+        <img :src="require(`../assets/svg/insta/svgexport-${svg}.svg`)" alt="logo">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      svg: 20,
+    };
+  },
+};
+</script>
+<style>
+  .header{
+    height: 54px;
+    width: 100%;
+  }
+  .container{
+    max-width: 975px;
+    min-width: auto;
+  }
+  .container-img{
+    height: 36px;
+    width: 228px;
+  }
+  .container-img img{
+    display: block;
+    height: 29px;
+    width: 103px;
+    min-height: auto;
+    max-width: 100%;
+    min-width: auto;
+  }
+
+  .container-search{
+    width: 215px;
+  }
+
+  .container-nav{
+    width: 222px;
+    height: 22px;
+    word-spacing: 0px;
+    text-decoration: none;
+    padding: 0 0 0 24px;
+  }
+</style>
