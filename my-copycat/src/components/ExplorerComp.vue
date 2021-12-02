303ffb7277d26529494e56a9a31a92c3b9da4968
Gaël <gaelduclocher.rougelot@gmail.com>
2021-12-02 16:53:56 +0100

    change Explorer page to a grid v1

--- my-copycat/src/components/ExplorerComp.vue
+++ my-copycat/src/components/ExplorerComp.vue
@@ -1,23 +1,20 @@
-<template lang="">
+<template>
   <div>
     <div class="main-container">
       <div class="content-container">
-
         <div class="div_large_right">
-          <div class="div_large_right_smalls_imgs">
-            <div class="small_imgs left_small">
-              <img src="" alt="">
-            </div>
-            <div class="small_imgs left_small">
-              <img src="" alt="">
-            </div>
+          <div class="div1">
+            <img src="" alt="" />
           </div>
-          <div class="large_imgs right_large">
-            <img src="" alt="">
+          <div class="div2">
+            <img src="" alt="" />
+          </div>
+          <div class="div3">
+            <img src="" alt="" />
           </div>
         </div>
 
-        <div class="div_small_images">
+        <!-- <div class="div_small_images">
           <div v-for="(img, index) in 6" :key="index" class="small_imgs left_small">
             <img src="" alt="">
           </div>
@@ -61,96 +58,106 @@
           <div v-for="(img, index) in 6" :key="index" class="small_imgs left_small">
             <img src="" alt="">
           </div>
-        </div>
-
+        </div> -->
       </div>
     </div>
   </div>
 </template>
 <script>
-export default {
-};
+export default {};
 </script>
 <style scoped>
-  @media (max-width: 415px) {
-    #footer {
-      display: block;
-    }
-  }
+.main-container {
+  width: 100vw;
+  height: 4000px;
+  display: flex;
+  justify-content: center;
+  position: absolute;
+}
+.content-container {
+  max-width: 975px;
+  width: 100%;
+  margin-top: 85px;
+  display: flex;
+  flex-direction: column;
+}
 
-  .main-container {
-    width: 100%;
-    height: 4000px;
-    display: flex;
-    justify-content: center;
-  }
-  .content-container {
-    max-width: 975px;
-    width: 100%;
-    margin-top: 85px;
-    display: flex;
-    flex-direction: column;
-  }
+/* IMAGE CONTAINERS*/
+.div_large_right {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  grid-column-gap: 28px;
+  grid-row-gap: 28px;
+  width: 100%;
+  max-height: 641.98px;
+  max-width: 963px;
+  height: 100%;
+  margin-bottom: 28px;
+}
 
-  /* IMAGE CONTAINERS*/
-  .div_large_right{
-    width: 100%;
-    max-height: 641.98px;
-    max-width: 963px;
-    height: 100%;
-    display: flex;
-  }
-  .div_large_left{
-    width: 100%;
-    max-height: 641.98px;
-    max-width: 963px;
-    height: 100%;
-    display: flex;
-    justify-content: flex-end;
-    flex-direction: row-reverse;
-  }
-  .div_large_right_smalls_imgs {
-    max-width: 292.98px;
-    width: 100%;
-  }
-  .div_small_images{
-    width: 100%;
-    max-height: 641.98px;
-    max-width: 963px;
-    height: 100%;
-    display: flex;
-    flex-wrap: wrap;
-  }
+.div1 {
+  grid-area: 1 / 1 / 3 / 2;
+  background: #000;
+}
+.div2 {
+  grid-area: 3 / 1 / 5 / 2;
+  background: #000;
+}
+.div3 {
+  grid-area: 1 / 2 / 5 / 4;
+  background: #000;
+}
 
-  /* IMAGE PROPERTIES */
-  /* Base properties */
-  .small_imgs {
-    max-width: 292.98px;
-    max-height: 292.98px;
-    height: 100%;
-    width: 100%;
-    background: rgb(189, 187, 187);
-    
-  }
-  .large_imgs {
-    max-width: 613.98px;
-    max-height: 613.98px;
-    height: 100%;
-    width: 100%;
-    background: rgb(189, 187, 187);
-  }
-  /* Modif large img left or Right */
-  .left_small{
-    margin:0 28px 28px 0;
-  }
-  .right_small{
-    margin:0 0 28px 28px;
-  }
-  .left_large{
-    margin-right: 0px;
-  }
-  .right_large{
-    margin-left: 28px;
-  }
+.div_large_left {
+  width: 100%;
+  max-height: 641.98px;
+  max-width: 963px;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: row-reverse;
+}
+.div_large_right_smalls_imgs {
+  max-width: 292.98px;
+  width: 100%;
+}
+.div_small_images {
+  width: 100%;
+  max-height: 641.98px;
+  max-width: 963px;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
 
+/* IMAGE PROPERTIES */
+/* Base properties */
+.small_imgs {
+  max-width: 292.98px;
+  max-height: 292.98px;
+  height: 100%;
+  width: 100%;
+  background: rgb(189, 187, 187);
+}
+.large_imgs {
+  max-width: 613.98px;
+  max-height: 613.98px;
+  height: 100%;
+  width: 100%;
+  background: rgb(189, 187, 187);
+}
+/* Modif large img left or Right */
+.left_small {
+  margin: 0 28px 28px 0;
+}
+.right_small {
+  margin: 0 0 28px 28px;
+}
+.left_large {
+  margin-right: 0px;
+}
+.right_large {
+  margin-left: 28px;
+}
 </style>
--- my-copycat/src/views/Explorer.vue
+++ my-copycat/src/views/Explorer.vue
@@ -1,5 +1,5 @@
-<template lang="">
-  <div>
+<template>
+  <div class="container">
     <Header/>
     <ExplorerComp/>
     <Footer id="footer"/>
@@ -7,14 +7,15 @@
 </template>
 <script>
 import Header from '../components/Header.vue';
-import Footer from '../components/Footer.vue';
 import ExplorerComp from '../components/ExplorerComp.vue';
+import Footer from '../components/Footer.vue';
 
 export default {
+  name: 'Explorer',
   components: {
     Header,
-    Footer,
     ExplorerComp,
+    Footer,
   },
 };
 </script>
@@ -23,5 +24,9 @@ export default {
     #footer {
       display: block;
     }
+  }
+  .container{
+    width: 100%;
+    height: 100%;
   }
 </style>
